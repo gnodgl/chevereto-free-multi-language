63f1715b493118f14fc1ca3b005996d09e1f81a7
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2022-01-07 13:25:58 -0300

    tags 1.6.1

--- app/install/installer.php
+++ app/install/installer.php
@@ -292,6 +292,8 @@ try {
         '1.5.0' => null,
         '1.5.1' => null,
         '1.6.0' => null,
+        '1.6.1' => null,
+        '1.6.2' => null,
     ];
     // Settings that must be renamed from NAME to NEW NAME and DELETE old NAME
     $settings_rename = [];
